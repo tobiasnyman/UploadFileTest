494bbc7b75593e4a2f3e4f5249a9bb371542b9f8
tobiasnyman <tobias.nyman@optistud.fi>
2016-04-13 13:07:13 +0200

    test

---
+++ UploadFileWithASPNETMVCtest/UploadFileWithASPNETMVCtest/Controllers/UploadFileController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UploadFileWithASPNETMVCtest.Controllers
+{
+    public class UploadFileController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public Action
+    }
+}
